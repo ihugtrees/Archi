72ce525760041c38945d4eab3b036318d38dc746
ihugtrees <igoralikin@gmail.com>
2020-03-14 17:26:14 +0200

    started

---
+++ Lab0/MyEcho/my_echo.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(int argc, char** argv) {
+  
+  char input
+  // fgets(str_buf, MAX_LEN, stdin);	/* get user input string */ 
+  printf("%s\n",argv[0]);
+  
+  return 0;
+}
