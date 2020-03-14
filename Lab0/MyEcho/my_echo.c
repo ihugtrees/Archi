20f89c8a7bfbd3ee903f7fcda44c4c90ba4a61bb
Igal <igoralikin@gmail.com>
2020-03-14 20:57:03 +0200

    fixed

--- Lab0/MyEcho/my_echo.c
+++ Lab0/MyEcho/my_echo.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 
-int main(int argc, char** argv) {
-  
-  char input
-  // fgets(str_buf, MAX_LEN, stdin);	/* get user input string */ 
-  printf("%s\n",argv[0]);
-  
+int main(int argc, char **argv)
+{
+
+  int i = 0;
+  //fgets(str_buf, MAX_LEN, stdin);	/* get user input string */
+  for (i = 1; i < argc; i++)
+  {
+    printf("%s ", argv[i]);
+  }
+
   return 0;
 }
