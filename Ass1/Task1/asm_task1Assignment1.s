089482a1db3aed72ef35be55087d58b5f538780c
ihugtrees <igoralikin@gmail.com>
2020-04-10 18:20:57 +0300

    finished task 1

--- Ass1/Task1/asm_task1Assignment1.s
+++ Ass1/Task1/asm_task1Assignment1.s
@@ -1,41 +1,39 @@
-section .data                    	; we define (global) initialized variables in .data section
-        an: dd 0             		; an is a local variable of size double-word, we use it to count the string characters
+
+section .rodata
+	format_string: db "%d",10,0 	; format string
 
 section .text                    	; we write code in .text section
-        global assFunc          		; 'global' directive causes the function do_Str(...) to appear in global scope
+        global assFunc          	; 'global' directive causes the function assFunc(...) to appear in global scope
+		extern c_checkValidity
+		extern printf
 
-assFunc:                        		; do_Str function definition - functions are defined as labels
+assFunc:                        	; assFunc function definition - functions are defined as labels
         push ebp              		; save Base Pointer (bp) original value
         mov ebp, esp         		; use Base Pointer to access stack contents (do_Str(...) activation frame)
         pushad                   	; push all signficant registers onto stack (backup registers values)
-        mov ecx, dword [ebp+8]		; get function argument on stack
-					; now ecx register points to the input string
-        mov dword [an], 0               ; initialize an variable with 0 value
-	yourCode:			; use label to build a loop for treating the input string characters
+        
+		mov ecx, dword [ebp+8]		; x argument
+		mov edx, dword [ebp+12]		; y argument
 
-                cmp byte [ecx], 0x20    ; Searching for spaces
-                je changeAndCount
-                cmp byte [ecx], 0x09    ; Searching for tabs
-                je changeAndCount
-                jmp ConLoop
-	
-        changeAndCount:
-                add byte [an], 1        ; incrementing the counter
-                mov byte [ecx], 0x5F
-
-        ConLoop:
-		inc ecx      	    	; increment ecx value; now ecx points to the next character of the string
-		cmp byte [ecx], 0   	; check if the next character (character = byte) is zero (i.e. null string termination)
-		jnz yourCode      	; if not, keep looping until meet null termination character
-                
+		push edx					; c_checkValidity second argument
+		push ecx					; c_checkValidity first argument
+		xor eax, eax				; zero eax
+		call c_checkValidity
+		pop ecx
+		pop edx
+		cmp eax,'0'
+		je add_Number
+		sub ecx,edx
+		jmp after_Sub
+		add_Number:
+		add ecx,edx
+		after_Sub:
+		push ecx
+		push format_string
+		call printf
+		add esp, 8
 	
         popad                    	; restore all previously used registers
-        mov eax,[an]         		; return an (returned values are in eax)
-        mov esp, ebp			; free function activation frame
-        pop ebp				; restore Base Pointer previous value (to returnt to the activation frame of main(...))
-        ret			
-
-
-
-
-                                                                                                                                                                                                                                                               
+        mov esp, ebp				; free function activation frame
+        pop ebp						; restore Base Pointer previous value (to returnt to the activation frame of main(...))
+        ret
--- Ass1/Task1/main_task1Assignment1.c
+++ Ass1/Task1/main_task1Assignment1.c
@@ -1,26 +1,27 @@
 #include <stdio.h>
-#define MAX_LEN 256 /* maximal input string size */
+#define MAX_LEN 64 /* maximal input string size */
 
 extern void assFunc(int x, int y);
 
 extern char c_checkValidity(int x, int y){
     if(x>=y)
-        return 1;
-    return 0;
+        return '1';
+    return '0';
 }
 
 int main(int argc, char **argv)
 {
-    char str_x[MAX_LEN];
-    char str_y[MAX_LEN];
+    char str_int[MAX_LEN];
     int x;
     int y;
 
-    printf("Enter 2 integers seperated by enter");
-    fgets(str_x, MAX_LEN, stdin); /* get user input string */
-    fgets(str_y, MAX_LEN, stdin); /* get user input string */
-    sscanf(str_x, "%d %d", x, y);
-    assFunc(x, y); /* call your assembly function */
+    printf("Enter 2 integers\n");
+    fgets(str_int, MAX_LEN, stdin);
+    sscanf(str_int, "%d", &x);
+
+    fgets(str_int, MAX_LEN, stdin);
+    sscanf(str_int, "%d", &y);
+    assFunc(x, y); 
 
     return 0;
 }
