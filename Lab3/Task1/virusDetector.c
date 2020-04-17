8579cc4897d6d2aeb07f23a2c2a088496edfc222
ihugtrees <igoralikin@gmail.com>
2020-04-18 01:37:21 +0300

    finished lab3

--- Lab3/Task1/virusDetector.c
+++ Lab3/Task1/virusDetector.c
@@ -24,6 +24,7 @@ struct fun_desc
 };
 
 link *global_virus_list = NULL;
+//FILE *infected = NULL;
 
 void printVirus(virus *virus, FILE *output)
 {
@@ -55,23 +56,29 @@ void list_print(link *virus_list, FILE *output)
 
 link *list_append(link *virus_list, virus *data)
 /* Add a new link with the given data to the list 
-        (either at the end or the beginning, depending on what your TA tells you),
-        and return a pointer to the list (i.e., the first link in the list).
-        If the list is null - create a new entry and return a pointer to the entry. */
+		(either at the end or the beginning, depending on what your TA tells you),
+		and return a pointer to the list (i.e., the first link in the list).
+		If the list is null - create a new entry and return a pointer to the entry. */
 {
 	link *new_link = (link *)malloc(sizeof(link));
 	new_link->vir = data;
 	new_link->nextVirus = NULL;
 
-	if (virus_list == NULL)
-		return new_link;
-
-	else
-	{
+	if (virus_list != NULL)
 		new_link->nextVirus = virus_list;
-		return new_link;
+	return new_link;
+}
+
+void virus_free(virus *vir)
+{
+	if (vir != NULL)
+	{
+		if (vir->sig != NULL)
+			free(vir->sig);
+		free(vir);
 	}
 }
+
 void list_free(link *virus_list) /* Free the memory allocated by the list. */
 {
 	if (virus_list != NULL)
@@ -79,7 +86,7 @@ void list_free(link *virus_list) /* Free the memory allocated by the list. */
 		if (virus_list->nextVirus != NULL)
 			list_free(virus_list->nextVirus);
 		if (virus_list->vir != NULL)
-			free(virus_list->vir);
+			virus_free(virus_list->vir);
 		free(virus_list);
 	}
 }
@@ -94,6 +101,7 @@ virus *readVirus(FILE *input)
 		fread(vir->sig, vir->SigSize, 1, input);
 		return vir;
 	}
+	virus_free(vir);
 	return NULL;
 }
 
@@ -145,27 +153,68 @@ void detect_viruses()
 	const unsigned short file_size = 10000;
 	char buffer[file_size];
 	char path[256];
-	printf("Enter path or filename for suspected file: ");
+	printf("Enter path or filename for infecteded file: ");
 	fgets(path, 256, stdin);
 	path[strlen(path) - 1] = '\0';
 	printf("Path: %s\n", path);
 
-	FILE *suspect = fopen(path, "r");
-	if (suspect != NULL)
+	FILE *infected = fopen(path, "r");
+	if (infected != NULL)
 	{
-		fseek(suspect, 0, SEEK_END);
-		unsigned short size = ftell(suspect);
-		fseek(suspect, 0, SEEK_SET);
+		fseek(infected, 0, SEEK_END);
+		unsigned short size = ftell(infected);
+		fseek(infected, 0, SEEK_SET);
 
-		fread(buffer, 1, size, suspect);
+		fread(buffer, 1, size, infected);
 
 		detect_virus(buffer, size, global_virus_list);
-		fclose(suspect);
+		fclose(infected);
 	}
 	else
 	{
 		printf("Bad Path\n");
 	}
+}
+
+void kill_virus(char *fileName, int signitureOffset, int signitureSize)
+{
+	FILE *infected_file = fopen(fileName, "r+");
+
+	if (infected_file != NULL)
+	{
+		char doNothing[signitureSize];
+		for (size_t i = 0; i < signitureSize; i++)
+			doNothing[i] = 0x90;
+
+		fseek(infected_file, signitureOffset, SEEK_SET);
+		fwrite(doNothing, 1, signitureSize, infected_file);
+		fclose(infected_file);
+	}
+	else
+		printf("Bad Path");
+}
+
+void fix_file()
+{
+	char path[256], input[256];
+	int offset = -1, sigsize = 0;
+
+	printf("Enter path or filename of infected file: ");
+	fgets(path, 256, stdin);
+	path[strlen(path) - 1] = '\0';
+	printf("Path: %s\n", path);
+
+	printf("Enter offset: ");
+	fgets(input, 256, stdin);
+	offset = strtol(input, NULL, 10);
+	printf("Offset: %d\n", offset);
+
+	printf("Enter sigsize: ");
+	fgets(input, 256, stdin);
+	sigsize = strtol(input, NULL, 10);
+	printf("Sig size: %d\n", sigsize);
+
+	kill_virus(path, offset, sigsize);
 }
 
 void print_signatures()
@@ -182,7 +231,13 @@ void quit()
 
 int main(int argc, char **argv)
 {
-	struct fun_desc menu[] = {{"Load signatures", load_signatures}, {"Print signatures", print_signatures}, {"Detect viruses", detect_viruses}, {"Quit", quit}, {NULL, NULL}};
+	struct fun_desc menu[] = {{"Load signatures", load_signatures},
+							  {"Print signatures", print_signatures},
+							  {"Detect viruses", detect_viruses},
+							  {"Fix file", fix_file},
+							  {"Quit", quit},
+							  {NULL, NULL}};
+
 	int menuLen = sizeof(menu) / sizeof(menu[0]) - 1;
 	char userInput[256];
 	int userChoise;
