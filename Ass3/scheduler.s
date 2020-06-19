915bd7bcb0ab56fa032c1c6cae4a7982e144faf8
ihugtrees <igoralikin@gmail.com>
2020-06-19 21:39:01 +0300

    almost ass 3

--- Ass3/scheduler.s
+++ Ass3/scheduler.s
@@ -1,55 +1,163 @@
-
+%macro printwinn 1
+	pushad
+	push %1
+	push printwin
+	call printf
+	add esp,8
+	popad
+%endmacro
 
 section .data
-  	SPP equ 4 							; offset of pointer to co-routine stack in co-routine struct 
+	SPP equ 4 							; offset of pointer to co-routine stack in co-routine struct
+	rounds: dd 0
+	droneSteps: dd 0
+	minDestroyed: dd 0
+	alive: dd 0
+	printwin: db "The winner is drone: %d", 10, 0
+
 section .text                           ; functions from c libary
-  align 16
-	 global scheduler:function
-   extern resume
-   extern drone
-   extern printer
-   extern N
-   extern main
-   
-   extern printerCo
-   extern CORS
-   extern K
-    
-     scheduler:
-    xor edi, edi
-    xor esi, esi
-    xor eax, eax
-    mov edi, dword 0
-    schedulerLoop:
-      mov eax, [N]
-      shl eax, 2
-      cmp eax, esi
-      je initEsi
-      jmp next
-      initEsi:
-      xor esi, esi
-      
-      
-      next:
-      mov eax, [CORS]
-      add eax, esi
-      mov ebx, [eax]
-      ;mov ebx, [eax + SPP]
-      call resume
-      add esi, dword 4
-      inc edi
-      cmp edi, [K]
-      je gotoPrinter  
-      jmp schedulerLoop
-    
-    gotoPrinter:
-      mov eax, [CORS]
-      mov ecx,dword [printerCo]
-      add eax, ecx
-      mov ebx, [eax]
-      ;mov ebx, [eax + SPP]
-      call resume
-      xor edi, edi
-      jmp schedulerLoop
+	align 16
+	global scheduler:function
+	extern N
+	extern K
+	extern R
+	extern resume
+	extern drone
+	extern printer
+	extern main
+	extern printerCo
+	extern CORS
+	extern dronesArray
+	extern printf
+	extern endCo
 
+scheduler:
+	xor esi, esi ; drone offset
+	xor eax, eax ; drone address
 
+	schedulerLoop:
+		mov eax, [N]
+		shl eax, 2
+		cmp eax, esi
+		je roundComplete
+		jmp next
+
+	roundComplete: ; finished whole round
+	  xor esi, esi
+	  inc byte [rounds]
+	  mov edx, [rounds]
+	  cmp edx, [R]
+	  je checkIfGameOver
+
+	next:
+		mov eax, [CORS]
+		add eax, esi
+		mov ebx, [eax]
+
+		mov ecx, [dronesArray] ; current drone pointer
+		add ecx, esi
+        mov ecx, [ecx]
+		add esi, 4 ; next drone
+		mov ecx, dword [ecx + 36] ; dead flag
+		cmp ecx, 1 		; check if dead
+		je schedulerLoop
+		inc byte [alive]
+
+		call resume
+		inc byte [droneSteps]
+		mov edi, [droneSteps]
+		cmp edi, [K]
+		je gotoPrinter
+		jmp schedulerLoop
+
+	gotoPrinter:
+		mov eax, [CORS]
+		mov ecx, dword [printerCo]
+		add eax, ecx
+		mov ebx, [eax]
+		call resume
+		mov dword [droneSteps], 0
+		jmp schedulerLoop
+
+	checkIfGameOver:
+		xor eax, eax
+		xor ecx, ecx ; counter
+		xor edx, edx 
+
+		mov dword [rounds], 0
+		cmp byte [alive], 1
+		je gameEnded
+		mov dword [alive], 0
+		
+		mov eax, [dronesArray]
+        mov eax, [eax]
+		mov edx, dword [eax + 32]
+		mov [minDestroyed], edx
+
+		loserLoop:
+			cmp ecx, [N]
+			je endLoop
+            mov edi, ecx
+			shl edi, 2
+			mov eax, [dronesArray] ; current drone
+			add eax, edi
+            mov eax, [eax]
+			mov edx, dword [eax + 36] ; if dead
+			cmp edx, 1
+			je contLoop
+			mov edx, dword [eax + 32] ; how many targets destroyed
+			cmp edx, [minDestroyed]
+			jl loser
+
+			contLoop:
+			inc ecx
+			jmp loserLoop
+
+			loser:
+			mov [minDestroyed], edx
+			jmp contLoop
+
+		endLoop:
+			xor ecx, ecx
+			eliminationLoop:
+                mov edi, ecx
+                shl edi, 2
+                mov eax, [dronesArray] ; current drone
+                add eax, edi
+                mov eax, [eax]
+				mov edx, dword [eax + 36] ; if dead
+				cmp edx, 1
+				je contElimLoop
+				mov edx, dword [eax + 32] ; how many targets destroyed
+				cmp edx, [minDestroyed]
+				je dieNoob
+
+				contElimLoop:
+				inc ecx
+				jmp eliminationLoop
+
+				dieNoob:
+				mov dword [eax + 36], 1
+				jmp next
+
+	gameEnded:
+		xor ecx, ecx
+		winnerLoop:
+            mov edi, ecx
+			shl edi, 2
+			mov eax, [dronesArray] ; current drone
+			add eax, edi
+            mov eax, [eax]
+			mov edx, dword [eax + 36] ; if dead
+			cmp edx, 1
+			je contWinLoop
+			jmp winner
+
+			contWinLoop:
+			inc ecx
+			jmp winnerLoop
+	winner:
+		inc ecx
+		printwinn ecx
+		ffree
+		call endCo
--- Ass3/target.s
+++ Ass3/target.s
@@ -1,74 +1,49 @@
-
 %macro generate_num 2
-        push %1
-        push %2
-        call random_number
-        add esp,8
+	push %1
+	push %2
+	call random_number
+	add esp,8
 %endmacro
-%macro print_float 1
-    pushad
-    fld qword [%1]
-    sub esp,8
-    fstp qword [esp]
-    push format_string_2f
-    call printf
-    add esp,12
-    popad
-    %endmacro
-
-
-
 
 section .text                           ; functions from c libary
-  align 16
-	 global target:function
-     global xt
-     global yt
-	 extern res
-	 extern CORS
-	 extern schedulerCo
-	 extern resume
-     extern drone
-     extern scheduler
-     extern printer
-     extern random_number
-     extern printf 
-     extern fprintf
-     extern sscanf
-     extern malloc
-     extern free
+	align 16
+	global target:function
+	global xt
+	global yt
+	extern res
+	extern CORS
+	extern schedulerCo
+	extern resume
+	extern drone
+	extern scheduler
+	extern printer
+	extern random_number
+	extern printf
+	extern fprintf
+	extern sscanf
+	extern malloc
+	extern free
 
-
-     target:
-     finit
-   	 xor edx,edx
-        
-   	 generate_num edx,distance
-        
-   	 fld qword [res]
-   	 fstp qword [xt]
-   	 generate_num edx,distance  
-   	 fld qword [res]
-   	 fstp qword [yt]
-   	 xor ebx,ebx
-   	 xor eax,eax
-   	 xor esi,esi
-   	 mov eax,[CORS]
-   	 mov esi,dword [schedulerCo]
-   	 add eax,esi
-   	 mov ebx, [eax]
-   	 call resume
-	 jmp target
-     
-
-
-
+target:
+	finit
+	xor edx,edx
+	generate_num edx,distance
+	fld qword [res]
+	fstp qword [xt]
+	generate_num edx,distance
+	fld qword [res]
+	fstp qword [yt]
+	xor ebx,ebx
+	xor eax,eax
+	xor esi,esi
+	mov eax,[CORS]
+	mov esi,dword [schedulerCo]
+	add eax,esi
+	mov ebx, [eax]
+	call resume
+	jmp target
 
 section .data
 	xt :dq 0
 	yt :dq 0
 	distance :equ 100
-	format_string_2f: db "%.2f",10,0 ; float 2 numbers after dot
-   
-
-
